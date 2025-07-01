f1e7838815a9bca99c7bdda1a872b47a1b16d64f
JasnaMia <bonsoirmia@yahoo.com.ar>
2025-07-01 18:37:04 -0300

    primer codigo

---
+++ main.js
@@ -0,0 +1,6 @@
+let etiquetaVar = document.getElementById("texto");
+//console.log(etiquetaVar.textContent);
+etiquetaVar.textContent = etiquetaVar.textContent + " y modificado";
+
+//console.log("hola");
+console.log(etiquetaVar);
