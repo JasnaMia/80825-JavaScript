a7bfb80d13af678865c9df791f70575048bab1d1
JasnaMia <bonsoirmia@yahoo.com.ar>
2025-07-03 12:21:04 -0300

    clase2

--- main.js
+++ main.js
@@ -4,3 +4,30 @@ etiquetaVar.textContent = etiquetaVar.textContent + " y modificado";
 
 //console.log("hola");
 console.log(etiquetaVar);
+
+
+// clase 2 - funciones, parametros
+let boton2 = document.getElementById("boton_clase2").addEventListener("click", function() {
+    let resultado = EjemploFuncion(5, 13);
+    console.log(`El resultado de la sumatoria es ${resultado}`)
+
+    console.log(`funcion flecha Ejemplo ${RetornaResultadoFlecha(6,5)}`)
+
+    UsandoCondicionales_Validar();
+})
+
+function EjemploFuncion(a, b){
+    return a + b;
+}
+
+let RetornaResultadoFlecha = (c, d) => {
+    return c * d;
+}
+
+function UsandoCondicionales_Validar() {
+    let numero = prompt("Ingrese un valor");
+    while (isNaN(numero)){
+        numero = prompt("Ingrese un valor numerico");
+    }
+    alert("buenisimo que ingresaste un valor valido");
+}
