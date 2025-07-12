4d5b17ac25f167c94c6a71e9798ee441a931ab35
JasnaMia <bonsoirmia@yahoo.com.ar>
2025-07-12 18:19:27 -0300

    class adicional

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
+
 let etiquetaVar = document.getElementById("texto");
 //console.log(etiquetaVar.textContent);
 etiquetaVar.textContent = etiquetaVar.textContent + " y modificado";
 
 //console.log("hola");
-console.log(etiquetaVar);
+//console.log(etiquetaVar);
 
 
 
-
+let total = 0;
 // clase 2 - funciones, parametros
-let boton2 = document.getElementById("boton_clase2").addEventListener("click", function() {
+let boton2 = document.getElementById("boton_clase2") 
+boton2.addEventListener("click", functionDelBoton2())
+
+
+// clase 3 - funciones, parametros
+let boton3 = document.getElementById("boton_clase3")
+boton3.addEventListener("click", functionDelBoton3())
+
+
+function functionDelBoton3(){
+    llamadaMenu();
+    while (confirm("Desea seguir comprando?")) {
+        llamadaMenu();
+    } 
+    alert (`El total es ${total}`)
+    total = 0;
+}
+
+function functionDelBoton2(){
+    let CambiamosTexto = document.getElementById('parrafoCambia')
     let resultado = EjemploFuncion(5, 13);
     console.log(`El resultado de la sumatoria es ${resultado}`)
     console.log(`funcion flecha Ejemplo ${RetornaResultadoFlecha(6,5)}`)
 
     UsandoCondicionales_Validar();
-})
+    CambiamosTexto.innerHTML = "click2"
+}
 
 function EjemploFuncion(a, b){
     return a + b;
@@ -33,19 +54,6 @@ function UsandoCondicionales_Validar() {
     alert("buenisimo que ingresaste un valor valido");
 }
 
-
-
-
-// clase 3 - funciones, parametros
-let total = 0;
-let boton3 = document.getElementById("boton_clase3").addEventListener("click", function() {
-    llamadaMenu();
-    while (confirm("Desea seguir comprando?")) {
-        llamadaMenu();
-    } 
-    alert (`El total es ${total}`)
-    total = 0;
-})
 
 function llamadaMenu(){
     let producto = Number(prompt("Bienvenidos a la tienda OnLine de Gquil, que desea comprar? \n 1. lapiz $3 \n 2. pluma $4 \n 3. marcador $5 \n 4. tijeras $6"));
